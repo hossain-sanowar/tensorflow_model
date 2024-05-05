e349888ac4a60351809b3d418252f990c8e26cf1
hossain-sanowar <md.sanowar21@gmail.com>
2024-05-06 00:02:28 +0200

    add normalize and improved accuracy

--- sequential_mnist.py
+++ sequential_mnist.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.datasets import mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model=Sequential([
     Flatten(input_shape=(28,28)),
